e6fb2ad7b227c9f0a5b4502b7c4e12199789f050
Kristo Palmik <kristo.palmik@gmail.com>
2024-04-04 17:14:41 +0300

    kerge yl

--- kodutoo2/kristo/kristo-yl3.py
+++ kodutoo2/kristo/kristo-yl3.py
@@ -1,1 +1,20 @@
+# leetcode ylesanne https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/
+# 1614. Maximum Nesting Depth of the Parentheses
 
+# s = "(1)+((2))+(((3)))"
+s = "(1+(2*3)+((8)/4))+1"
+def maxDepth(s):
+    current_depth = 0
+    very_deep = 0
+
+    for char in s:
+        if char == "(":
+            current_depth += 1
+            very_deep = max(very_deep, current_depth)
+        elif char == ")":
+            current_depth -= 1
+
+    return very_deep
+
+sugavus = maxDepth(s)
+print("Koige sugavam arv on: " + str(sugavus))
